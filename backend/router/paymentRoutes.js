9c9a10c26919d2463628cc079e72da281f8bf44b
Ayush navneet singh <ayushnavneetsingh@gmail.com>
2025-09-13 16:40:40 +0530

    first commit

---
+++ backend/router/paymentRoutes.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+// const {checkOut , paymentVerification} = require("../controller/paymentController")
+// const paymentRouter = express.Router();
+
+
+// paymentRouter.post('/checkOut' , checkOut);
+// paymentRouter.post('/verification' , paymentVerification);
+
+// module.exports = paymentRouter;
